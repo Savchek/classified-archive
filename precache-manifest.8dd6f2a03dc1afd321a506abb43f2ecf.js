ccaed7c8739b1ff4ecf290a10c2bbb0ed8b1faff
Savchek <danil.753253@gmail.com>
2020-09-02 19:29:48 +0300

    Updates

---
+++ precache-manifest.8dd6f2a03dc1afd321a506abb43f2ecf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d12733c2f6646fc3b22e774b9122064",
+    "url": "/classified-archive/index.html"
+  },
+  {
+    "revision": "db3a244df60285425bcf",
+    "url": "/classified-archive/static/css/main.3f37865c.chunk.css"
+  },
+  {
+    "revision": "03b18f3020760f7c9551",
+    "url": "/classified-archive/static/js/2.9a132d4f.chunk.js"
+  },
+  {
+    "revision": "ad6d24dd93f253e6c294bdcb2f16d72d",
+    "url": "/classified-archive/static/js/2.9a132d4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db3a244df60285425bcf",
+    "url": "/classified-archive/static/js/main.4a6f0fd9.chunk.js"
+  },
+  {
+    "revision": "466b507c3d8d96d49433",
+    "url": "/classified-archive/static/js/runtime-main.9c473d9d.js"
+  }
+]);
